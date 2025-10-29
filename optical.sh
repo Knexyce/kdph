d1e72c7831d37ae0464ec427d5d5ed4591f5368e
Knexyce <knexyce@gmail.com>
2025-10-29 18:25:12 -0400

    Add files via upload

---
+++ optical.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Optical Package Manager
+
+# More user friendly frontend for KDPH.
+# System level package manager with dependency resolution, package building, and configuring by using a configuration file ("optcfg.py") in packages.
+# Powered by KDPH engine and retains the same features.
+
+curl https://raw.githubusercontent.com/Knexyce/kdph/main/kdph.py -o kdph.py
+python3 kdph.py getpkg -a k-auto -p optical -k kp
+cd optical
+./setup.sh
+cd ~
+
+echo "Optical has been installed."
+
+# Author: Ayan Alam (Knexyce).
+# All rights regarding this software are reserved by Knexyce only.
